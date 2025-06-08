1b6177f97f334e660fa08e1b126b1937102d8936
Lorrah <lorrahngobeni.ln@gmail.com>
2025-06-09 00:35:31 +0200

    Time string conversion

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -13,6 +13,7 @@ def calculate_future_date():
     current_date = datetime.date.today()
     days_ahead = timedelta(days= number_of_days)
     future_date = current_date + days_ahead
+    future_date = future_date.strftime("%Y-%m-%d")
     print("Future date:",future_date)
     
 calculate_future_date()
