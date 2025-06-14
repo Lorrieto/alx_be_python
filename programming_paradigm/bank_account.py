06e6c934c54408867e87994b3c8433f361584e54
Lorrah <lorrahngobeni.ln@gmail.com>
2025-06-15 00:34:05 +0200

    Bank Account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.deposit_ = self.amount + account_balance 
+        return self.deposit_
+
+    def withdraw(self, amount):
+        self.withdrawal_ = account_balance - self.amount
+        return self.withdrawal_
+    def display_balance(self):
+        print("dffg", account_balance)
+        
+
+
+
