adda5651c9d459154f0373bb2e8f1370f99a3200
Lorrah <lorrahngobeni.ln@gmail.com>
2025-06-15 01:10:06 +0200

    Robust Division Calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,16 @@
+
+    
+def safe_divide(numerator, denominator):
+    division_= float(numerator/denominator)
+    return division_
+
+try:
+    print(" ")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+
+except ValueEror:
+    ("Please enter a numeric value")
+    
+    
