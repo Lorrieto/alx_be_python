1d9fb27eb52f5377231bb3007d1a38e178a1dc6b
Lorrah <lorrahngobeni.ln@gmail.com>
2025-06-16 01:55:28 +0200

    Changed Class call name

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,20 +4,20 @@ from simple_calculator import SimpleCalculator
 class TestClass(unittest.TestCase):
 
     def testClass(self):
-        self.testClass = SimpleCalculator()
+        self.calc = SimpleCalculator()
         
 
     def test_addition(self):
-        self.assertEqual(self.testClass.add(5,4),9)
+        self.assertEqual(self.calc.add(5,4),9)
 
     def test_subtraction(self):
-        self.assertEqual(self.testClass.subtract(8,10),-2)
+        self.assertEqual(self.calc.subtract(8,10),-2)
 
     def test_multiplication(self):
-        self.assertEqual(self.testClass.multiply(15,3),5)
+        self.assertEqual(self.calc.multiply(15,3),5)
 
     def test_Division(self):
-        self.assertEqual(self.testClass.divide(9,3),3)
+        self.assertEqual(self.calc.divide(9,3),3)
         with self.assertRaises(ValueError):
-            self.testClass.divide(10,0)
+            self.calc.divide(10,0)
             
