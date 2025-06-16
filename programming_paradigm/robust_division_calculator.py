0ac8846bd5693f9d9759974a48a9f4fee4d0a518
Lorrah <lorrahngobeni.ln@gmail.com>
2025-06-17 01:34:40 +0200

    Robust division with exception handling

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
         result = float(numerator)/ float(denominator)
-        return "The result of the division is", division
+        return "The result of the division is", result
         
     except ZeroDivisionError:
        print("Error: Cannot divide by zero.")
