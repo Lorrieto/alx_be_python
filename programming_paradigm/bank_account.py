417d0745048ca2eec9aae99f87937996743eaa3c
Lorrah <lorrahngobeni.ln@gmail.com>
2025-06-17 00:33:46 +0200

    Corrected deposit and withdrawal calculations

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,11 +1,17 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
-        self.account_balance = account_balance
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
-        self.amount += self.account_balance 
+        if amount > 0:
+          amount += self.account_balance
+
+        else:
+            print("Deposit amount must be negative")
+        
 
     def withdraw(self, amount):
+        
         if self.account_balance < self.amount:
             self.amount -= self.account_balance
             return True
