c2d314ecc6092a5f0b7827fb753555320fe93f94
Lorrah <lorrahngobeni.ln@gmail.com>
2025-06-27 02:08:54 +0200

    Area polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,20 @@
+import math
+
+class Shape:
+
+    def area(self):
+        "NotImplementedError"
+
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        self.length * self.width
+
+class Circle(Shape):
+     def __init__(self, radius):
+         self.radius = radius
+         math.pi* self.radius * self.radius
+    
+    
